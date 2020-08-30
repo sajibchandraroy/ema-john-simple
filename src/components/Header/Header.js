33eca6e83c420d427fde590b1bc5489c9e640f01
Sumita5 <satyakiroyhreeg@gmail.com>
2020-08-30 23:20:47 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt=""/>
+            
+        </div>
+    );
+};
+
+export default Header;
