87ba7814c34869c9cf0a5da9eef74dbf6e9f0188
Sumita5 <satyakiroyhreeg@gmail.com>
2020-09-11 23:56:44 +0600

    full okay but so need some work

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const inventory = () => {
+    return (
+        <div>
+            <h1>This is an inventory</h1>
+        </div>
+    );
+};
+
+export default inventory;
